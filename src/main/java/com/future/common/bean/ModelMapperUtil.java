b270329a60f16dd76c241cedf8a091aa9b43226a
dexterleslie@gmail.com <dexterleslie@gmail.com>
2024-05-29 11:44:05 +0800

    新增ModelMapperUtil

---
+++ src/main/java/com/future/common/bean/ModelMapperUtil.java
@@ -0,0 +1,7 @@
+package com.future.common.bean;
+
+import org.modelmapper.ModelMapper;
+
+public class ModelMapperUtil {
+    public static final ModelMapper ModelMapperInstance = new ModelMapper();
+}
